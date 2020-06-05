8119746dee82e97224300827e65d0711a926cec6
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2020-06-05 14:52:22 -0400

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -1,3 +1,8 @@
+rovider "aws" {
+  region     = "us-west-2"
+ access_key = "AKIA3FRW5ZXDE5VYNKOW"
+  secret_key = "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"
+}
 resource "aws_instance" "myec2"{
   ami = "ami-0e34e7b9ca0ace12d"
   instance_type = "t2.micro"
