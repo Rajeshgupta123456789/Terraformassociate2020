fdab6deb1e4e0ac4f93f9af85ee336124de177da
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2020-06-05 14:27:06 -0400

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "us-west-2"
- access_key = "AKIA3FRW5ZXDE5VYNKOW"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2"{
   ami = "ami-0d6621c01e8c2de2c"
   instance_type = "t2.micro"
